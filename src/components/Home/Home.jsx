5461bd0d3158dbf1b21724e1e0d1b314eddb857c
Jonathas <44008790+jxnathas@users.noreply.github.com>
2025-04-21 10:46:38 -0300

    Add Home and NavBar components with styling
    - Created Home component with associated CSS for layout and styling.
    - Added social media icons and links in Home component.
    - Implemented NavBar component with navigation links and logo.
    - Styled NavBar for fixed positioning and responsive design.

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Home.css';
+import { FaGithub, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
+
+const Home = () => {
+    return (
+        <section className="home" id='home'>
+            <div className="home-container">
+                <img src="https://avatars.githubusercontent.com/u/44008790?v=4" alt="img" className="home-img" />
+                <h1 className='name'>Jonathas Santos</h1>
+                <span className="home-subtitle">Software Engineer</span>
+                <div className="home-socials">
+                    <a href="linkedin.com/in/jxnathas/" target="_blank" rel="noopener noreferrer" className="home-social-link"><FaLinkedinIn /></a>
+                    <a href="github.com/jxnathas" target="_blank" rel="noopener noreferrer" className="home-social-link"><FaGithub/></a>
+                    <a href="https://www.instagram.com/_jnths/" target="_blank" rel="noopener noreferrer" className="home-social-link"><FaInstagram/></a>
+                </div>
+                <button className="home-button">Talk with me!</button>
+            </div>
+        </section>
+    );
+};
+
+export default Home;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './NavBar.css';
+import logo from '../../assets/logo.png';
+import { FaHome, FaUserCheck, FaEnvelope, FaBlackTie, FaUserGraduate } from 'react-icons/fa';
+
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <img src={logo} alt="Logo" className="navbar-logo-img" />
+            </div>
+            <ul className="navbar-list">
+                <li className="navbar-item">
+                    <a href="#home">
+                        <FaHome className="navbar-icon" />
+                    </a>
+                </li>
+                <li className="navbar-item">
+                    <a href="#about">
+                        <FaUserCheck className="navbar-icon" />
+                    </a>
+                </li>
+                <li className="navbar-item">
+                    <a href="#services">
+                        <FaBlackTie className="navbar-icon" />
+                    </a>
+                </li>
+                <li className="navbar-item">
+                    <a href="#resume">
+                        <FaUserGraduate className="navbar-icon" />
+                    </a>
+                </li>
+                <li className="navbar-item">
+                    <a href="#contact">
+                        <FaEnvelope className="navbar-icon" />
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
